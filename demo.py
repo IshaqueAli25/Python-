16f2c7b329eeb5d7eb0f6c293887a60552c3619a
IshaqueAli25 <mirishaqueali1@gmail.com>
2025-11-14 11:12:38 +0000

    revising

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 print("hello world")
-
+# for creating local account 'git clone'
 # for adding to local 'git add .'
 #for checking updates 'git status'
-# for puch 'git push origin main'
+
+# for commiting 'git commit -m'this is message/comment on the files pushed'
+# for push 'git push origin main'
